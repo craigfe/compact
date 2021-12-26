4e0ef18325813c915abb361af1401f7c136d8a49
Craig Ferguson <me@craigfe.io>
2021-12-26 17:24:46 +0000

    Add test for adding non-immediate value to Hashset.Immediate

--- test/test_hashed_container.ml
+++ test/test_hashed_container.ml
@@ -1,27 +1,47 @@
 open! Compact
 
-module Key = struct
+module Int_key = struct
   include Stdlib.Int
 
   let hash = Stdlib.Hashtbl.hash
   let hash_size = 30
 end
 
-let test_mutation_while_iterating () =
-  let t = Hashset.create (module Key) ~initial_capacity:1 in
-  Hashset.add t 1;
+module String_key = struct
+  include Stdlib.String
 
-  let expect_failure pos ~f =
-    match f () with
-    | exception Failure _ -> ()
-    | exception _ -> assert false
-    | _ ->
-        (* | (exception _) -> *)
-        Alcotest.fail ~pos "Expected mutation while iterating to raise Failure"
-  in
+  let hash = Stdlib.Hashtbl.hash
+  let hash_size = 30
+end
+
+let expect_failure ?(msg = "") pos ~f =
+  match f () with
+  | exception Failure _ -> ()
+  | exception _ -> assert false
+  | _ -> Alcotest.fail ~pos msg
+
+let test_non_immediate_entry () =
+  let msg = "Expected adding a non-immediate value to raise Failure" in
+
+  let module T = Hashset.Immediate in
+  let t = T.create (module String_key) ~initial_capacity:0 in
+  expect_failure __POS__ ~msg ~f:(fun () ->
+      T.add t "strings are not immediates");
+
+  let module T = Hashset.Immediate64 in
+  let t = T.create (module String_key) ~initial_capacity:0 in
+  expect_failure __POS__ ~msg ~f:(fun () ->
+      T.add t "strings are not immediates")
+
+let test_mutation_while_iterating () =
+  let t = Hashset.create (module Int_key) ~initial_capacity:1 in
+  Hashset.add t 1;
 
   (* Attempting a mutation during an iteration is forbidden: even if the
      mutation has no effect (e.g. we add an element that already exists): *)
+  let expect_failure =
+    expect_failure ~msg:"Expected mutation while iterating to raise Failure"
+  in
   let expect_fail_in_iter pos ~f =
     Hashset.iter t ~f:(function
       | 1 -> expect_failure pos ~f
@@ -50,4 +70,6 @@ let test_mutation_while_iterating () =
 
 let tests =
   let test name fn = Alcotest.test_case name `Quick fn in
-  [ test "Hashset.mutation_while_iterating" test_mutation_while_iterating ]
+  [ test "Hashset.Immediate.non_immediate_entry" test_non_immediate_entry
+  ; test "Hashset.mutation_while_iterating" test_mutation_while_iterating
+  ]
